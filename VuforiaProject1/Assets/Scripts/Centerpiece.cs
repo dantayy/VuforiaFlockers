a8c58c1885f9e5e22ba6562e58c972c1115ad78d
dantayy <nvm6501@rit.edu>
2020-02-17 23:19:52 -0500

    Integrated Vuforia and began to get flockers to flock around centerpiece generated when ground plane is found

---
+++ VuforiaProject1/Assets/Scripts/Centerpiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//class for general scene management
+//includes attributes/methods for debug lines, vehicle instantiation, and flow-field creation
+public class Centerpiece : MonoBehaviour
+{
+    //attributes
+    public GameObject flocker;
+    public int numFlockers;
+
+    void OnEnable()
+    {
+        // spawn in the flockers
+        for (int i = 0; i < numFlockers; i++)
+        {
+            Instantiate(flocker, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10)), new Quaternion());
+        }
+    }
+
+    void OnDisable()
+    {
+        // despawn flockers
+        foreach (GameObject flocker in GameObject.FindGameObjectsWithTag("Flocker"))
+        {
+            Destroy(flocker);
+        }
+    }
+}
